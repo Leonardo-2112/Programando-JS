884b99f9ad5530a849e67702c23c7d46f720a335
Leonardo-2112 <afoncoleonardo21@gmail.com>
2026-08-01 19:14:29 -0300

    Adicionando aula 05 do quarto curso de JS para FrontEnd

--- Trilha-FrontEnd/curso04/script.js
+++ Trilha-FrontEnd/curso04/script.js
@@ -7,14 +7,12 @@ const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button ');
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const startPauseBt = document.querySelector('#start-pause');
+const iniciarOuPausarBt = document.querySelector('#start-pause span');
+const imgComecar = document.querySelector('.app__card-primary-butto-icon');
+const tempoNaTela = document.getElementById('timer');
 
 let intervaloId = null
-let tempoDecorridoEmSegundos = 5;
-
-const timer = document.getElementById('timer');
-const duracaoFoco = 1500; 
-const duracaoDescansoCurto = 300; 
-const duracaoDescansoLongo = 900; 
+let tempoDecorridoEmSegundos = 1500;
 
 //Cria uma instancia do Objeto Audio
 const musica = new Audio('sons/luna-rise-part-one.mp3');
@@ -40,6 +38,7 @@ musicaFocoInput.addEventListener('change', () => {
 
 //Quando clica no botão de foco altera o atributo para "classe" foco
 focoBt.addEventListener('click', () =>{
+    tempoDecorridoEmSegundos = 1500;
     //Chama a função para alterar o contexto, passando o valor como parametro
     alterarContexto('foco');
     focoBt.classList.add('active');
@@ -48,6 +47,8 @@ focoBt.addEventListener('click', () =>{
 
 //Quando clica no botão de foco altera o atributo para "classe" descanso-curto
 curtoBt.addEventListener('click', () => {
+    //Altera o tempo do timer de acordo com o lugar que está
+    tempoDecorridoEmSegundos = 300;
     alterarContexto('descanso-curto');
     //classList ->  propriedade de leitura que serve para ver e mexer nas classes de um elemento HTML
     curtoBt.classList.add('active');
@@ -55,13 +56,15 @@ curtoBt.addEventListener('click', () => {
 
 //Quando clica no botão de foco altera o atributo para "classe" descanso-longo
 longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900;
     alterarContexto('descanso-longo');
     longoBt.classList.add('active');
 });
 
 //Substitui o valor do atributo e o caminho a imagem de acordo com o contexto(parametro) passado
 function alterarContexto(contexto){
-    
+    //Chama função para mostrar o tempo toda vez que alterar o contexto
+    mostrarTempo();
     //Percorre o array de botôes removendo o active de todos
     //(como dentro dos eventos a função é chamada antes do classList.add funciona perfeitamente)
     botoes.forEach(function (contexto){
@@ -95,12 +98,12 @@ function alterarContexto(contexto){
 const contagemRegressiva = () =>{
     if(tempoDecorridoEmSegundos <= 0){
         musicaAcabouTempo.play();
-        zerar();
         alert('Tempo Finalizado');
+        zerar();
         return
     }
     tempoDecorridoEmSegundos -= 1;
-    console.log('Temporizador: ' + tempoDecorridoEmSegundos);
+    mostrarTempo();
 };
 
 function iniciarOuPausar(){
@@ -112,6 +115,9 @@ function iniciarOuPausar(){
     musicaPlay.play();
     //setInterval -> 1 parametro oque ele vai executar, 2 parametro a cada quando tempo ele vai executra isto (em milisegundos)
     intervaloId = setInterval(contagemRegressiva, 1000);
+    //textContent -> inserir texto (não entende com tag HTML)
+    iniciarOuPausarBt.textContent = "Pausar";
+    imgComecar.setAttribute("src", "imagens/pause.png");
 }
 
 startPauseBt.addEventListener('click', iniciarOuPausar);
@@ -119,5 +125,19 @@ startPauseBt.addEventListener('click', iniciarOuPausar);
 function zerar(){
     //clearInterval -> interrompe um comando
     clearInterval(intervaloId);
+    //textContent -> inserir texto (não entende com tag HTML)
+    iniciarOuPausarBt.textContent = "Começar";
+    imgComecar.setAttribute("src", "imagens/play_arrow.png");
     intervaloId = null;
 }
+
+function mostrarTempo(){
+    //Instancia o objeto date e mutiplica para ficar em milisegundos
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000);
+    //Formata para aparecer em minutos
+    const tempoFormatado = tempo.toLocaleTimeString("pt-BR", {minute: '2-digit', second: '2-digit'});
+    //Exibe na tela chamando o elemento HTML atribuido na variavel "tempoNaTela"
+    tempoNaTela.innerHTML = `${tempoFormatado}`;
+}
+//Inicia com o timer na Tela
+mostrarTempo();
