d6e6c5f4cbef72380d0d37c4b08a36e8a8a24fd6
Leonardo-2112 <afoncoleonardo21@gmail.com>
2026-08-01 23:05:51 -0300

    Adicionando aula 02 ao quinto curso de JS para FrontEnd

--- Trilha-FrontEnd/curso05/script-crud.js
+++ Trilha-FrontEnd/curso05/script-crud.js
@@ -3,8 +3,43 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
 const textarea = document.querySelector('.app__form-textarea');
+const ulTarefas = document.querySelector('.app__section-task-list');
 
-const tarefas = [];
+//getItem -> método da localStorage que permite pegar valores de um lugar salvo
+const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []; //Caso n tenha valor para converter ele coloca um array vazio
+
+function criarElementoTarefa(tarefa) {
+    const li = document.createElement('li');
+    //Cria uma classe e atribui ela ao elemento HTML "li"
+    li.classList.add('app__section-task-list-item');
+
+    const svg = document.createElement('svg');
+    //Atribui um molde com classes de estilização para o elemento HTML "svg"
+    svg.innerHTML = `<svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+        </svg>`;
+
+    const paragrafo = document.createElement('p');
+    //Atribui o valor de descriçao ao elemento 'p'
+    paragrafo.textContent = tarefa.descricao;
+    paragrafo.classList.add('app__section-task-list-item-description');
+    
+    const botao = document.createElement('button');
+    botao.classList.add('app_button-edit');
+    
+    const imagemBotao = document.createElement('img');
+    //Passa como valor da imagem o caminho dela
+    imagemBotao.setAttribute('src', 'imagens/edit.png');
+    //coloca a imagem dentro do botão
+    botao.append(imagemBotao);
+    //coloca os elementos criados dentro da li
+    li.append(svg)
+    li.append(paragrafo)
+    li.append(botao)
+    
+    return li;
+}
 
 btnAdicionarTarefa.addEventListener('click', () => {
     //toggle -> adiciona a classe CSS ao elemento se ela não existir, ou a remove se ela já estiver presente, 
@@ -18,11 +53,26 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
     evento.preventDefault();
     //cria um objeto tarefa com a propriedade descricao cujo o valor é o valor do textarea escrito pelo usuário
     const tarefa = {
-        decricao: textarea.value
+        descricao: textarea.value
     }
     //Coloca o objeto tarefa dentro da lista(array) tarefas
     tarefas.push(tarefa);
+    //Pegando tarefa adicionada e colocando dentro da lista no HTML
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa);
+
     //localStorage ->  Os dados continuam salvos mesmo se você fechar a aba (só salva strings)
     //(onde irá salvar, oque deve salvar)
     localStorage.setItem('tarefas', JSON.stringify(tarefas));//convertendo objeto em strings usando JSON
+
+    //Limpando textarea e escondendo form após adicionar tarefa nova
+    textarea.value = '';
+    formAdicionarTarefa.classList.add('hidden');
+});
+
+//Percorre o array de tarefas e exibe cada uma
+tarefas.forEach(tarefa => {
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    //Coloca o elemento criado dentro da ul
+    ulTarefas.append(elementoTarefa);
 });
